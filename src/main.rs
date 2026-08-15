ae3e397455a322492e29d08f5cf6462930d1b159
Wyatt Lamberth <wyattlamberth@outlook.com>
2026-08-15 10:00:44 -0500

    Take command line arg instead of hardcode filepath

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 use std::fs;
 use std::error::Error;
+use std::env;
 
 fn main() {
-    let message: String = fs::read_to_string("practice.txt").unwrap();
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 2 { // quick check we dont have too many
+        println!{"Too many arguments!\nOnly accepted argument is a single filepath."}
+        // to be fair, could just ignore the rest instead of returning.
+        // TODO ignore rest of args instead of return
+        return;
+    }
+    // TODO use .get() and handle the optional value
+    let filepath = &args[1];
+    let message: String = fs::read_to_string(filepath).unwrap();
     let mut line_count = 0;
     for line in message.lines() {
         println!("Line: {}", line);
