fb06d3643178627f80aea585b5e2c04ab450a6a1
Wyatt Lamberth <wyattlamberth@outlook.com>
2026-08-15 09:49:07 -0500

    Printing some stats about the file

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,17 @@ use std::error::Error;
 
 fn main() {
     let message: String = fs::read_to_string("practice.txt").unwrap();
-    println!("{}", message);
+    let mut line_count = 0;
+    for line in message.lines() {
+        println!("Line: {}", line);
+        line_count += 1;
+    }
+    let mut word_count = 0;
+    for word in message.split_whitespace() {
+        println!("Word: {}", word);
+        word_count += 1;
+    }
+    println!("Line Count: {}", line_count);
+    println!("Word Count: {}", word_count);
+    println!("Entire Text: {}", message);
 }
